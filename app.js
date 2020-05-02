4fcd56ebcf5e80023a626c38e2b7af7b66fcaecc
CNT135 <64455309+CNT135@users.noreply.github.com>
2020-05-02 23:59:45 +0700

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const VnExpress = require('./news/vnexpress');
 const Facebook = require('./facebook');
 const VnExpressSports = require('./news/vnexpresssports');
 const FacebookSports = require('./facebooksports');
+const VnExpressEconomy = require('./news/vnexpresseconomy');
+const FacebookEconomy = require('./facebookeconomy');
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
@@ -80,6 +82,24 @@ const generatorSports = async () => {
     facebook.postNews(images);
 };
 
+const generatorEconomy = async () => {
+    let vnExpress = new VnExpressEconomy();
+    let news = await vnExpress.get();
+
+    let createImage = new CreateImage();
+    let images = [];
+    for (const feed of news) {
+        images.push(await createImage.create(feed.img, feed.title, feed.description));
+    }
+
+    let facebook = new FacebookEconomy(process.env.ACCESS_TOKEN, process.env.GROUP_ID);
+
+    if (process.env.PAGE_ID) {
+        facebook.postAsPage(process.env.PAGE_ID)
+    }
+    facebook.postNews(images);
+};
+
 app.use((req, res, next) => {
     //BASIC AUTHENTICATION
     const auth = {login: process.env.BASIC_USER, password: process.env.BASIC_PASSWORD};
@@ -96,22 +116,27 @@ app.use((req, res, next) => {
 
 app.get('/sang', (req, res) => {
     generatorsang(); //no need wait done and no need return anything
-    res.send('Đang thực hiện Điểm tin Sáng!')
+    res.send('Đang thực hiện ĐIỂM TIN SÁNG!')
 });
 
 app.get('/trua', (req, res) => {
     generatortrua(); //no need wait done and no need return anything
-    res.send('Đang thực hiện Điểm tin Trưa!')
+    res.send('Đang thực hiện ĐIỂM TIN TRƯA!')
 });
 
 app.get('/toi', (req, res) => {
     generatortoi(); //no need wait done and no need return anything
-    res.send('Đang thực hiện Điểm tin Cuối ngày!')
+    res.send('Đang thực hiện ĐIỂM TIN CUỐI NGÀY!')
 });
 
 app.get('/thethao', (req, res) => {
     generatorSports(); //no need wait done and no need return anything
-    res.send('Đang thực hiện Điểm tin Thể thao!')
+    res.send('Đang thực hiện ĐIỂM TIN THỂ THAO!')
+});
+
+app.get('/kinhte', (req, res) => {
+    generatorEconomy(); //no need wait done and no need return anything
+    res.send('Đang thực hiện ĐIỂM TIN TÀI CHÍNH KINH DOANH!')
 });
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
